3b6d1eb9a843b0f87ff462acd55a5d76537cc2ff
kollnerdani <dankollner@gmail.com>
2023-03-29 12:53:11 +0200

    opt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="categories-list">
-      <div className="category">
-        I am here
+      <div className="categories-container">
+          <div className="category-container">
+              {/*<img src="" alt=""/>*/}
+              <div className="category-body-container">
+                  <h2>Hats</h2>
+                  <p>Shop Now</p>
+              </div>
+          </div>
+          <div className="category-container">
+              {/*<img src="" alt=""/>*/}
+              <div className="category-body-container">
+                  <h2>Jackets</h2>
+                  <p>Shop Now</p>
+              </div>
+          </div>
+          <div className="category-container">
+              {/*<img src="" alt=""/>*/}
+              <div className="category-body-container">
+                  <h2>Sneakers</h2>
+                  <p>Shop Now</p>
+              </div>
+          </div>
+          <div className="category-container">
+              {/*<img src="" alt=""/>*/}
+              <div className="category-body-container">
+                  <h2>Womens</h2>
+                  <p>Shop Now</p>
+              </div>
+          </div>
+          <div className="category-container">
+              {/*<img src="" alt=""/>*/}
+              <div className="category-body-container">
+                  <h2>Mans</h2>
+                  <p>Shop Now</p>
+              </div>
+          </div>
       </div>
-    </div>
   );
 }
 
