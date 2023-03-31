6c1a030f60ded082d9bb51753df7394c76862b7c
kollnerdani <dankollner@gmail.com>
2023-03-31 13:55:32 +0200

    google redirect depredicted

--- src/routes/sign_in/sign_in.jsx
+++ src/routes/sign_in/sign_in.jsx
@@ -4,23 +4,15 @@ import { auth, signInWithGooglePopUp, signInWithGoogleRedirect ,createUserDocume
 
 
 const SignIn = () => {
-    useEffect( async () =>{
-        const response = await getRedirectResult(auth);
-        console.log(response)
-    }, [])
     const googleUser = async () => {
         const { user } = await signInWithGooglePopUp();
         const userDocRef = await createUserDocument(user);
     }
-
     return (
         <div>
             <h1>SIGN IN</h1>
             <button onClick={googleUser}>
                 Sign in with Google
-            </button>
-            <button onClick={signInWithGoogleRedirect}>
-                Sign in with GoogleRedirect
             </button>
         </div>
     )
