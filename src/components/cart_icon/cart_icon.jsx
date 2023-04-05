8a9eceab12e6239c4accd332042b8c125b355134
kollnerdani <dankollner@gmail.com>
2023-04-05 16:43:13 +0200

    cart counter optimized

--- src/components/cart_icon/cart_icon.jsx
+++ src/components/cart_icon/cart_icon.jsx
@@ -5,14 +5,13 @@ import './cart_icon.scss';
 
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext)
-    const { cartItems } = useContext(CartContext)
-    const currentValue = cartItems.map(cartItem=> cartItem.quantity).reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
+    console.log(cartCount)
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
     return(
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingCartIcon className='shopping-icon'/>
-            <span className='item-count'>{currentValue}</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     )
 }
