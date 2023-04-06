cc22500649604e58e490b806f7ab09712ce37abf
kollnerdani <dankollner@gmail.com>
2023-04-06 21:28:09 +0200

    preview

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import './index.scss';
 import App from './App';
 import { UserProvider } from "./contexts/user";
-import { ProductsProvider } from "./contexts/product";
+import { CategoriesProvider } from "./contexts/categories";
 import { CartProvider } from "./contexts/cart";
 import reportWebVitals from './reportWebVitals';
 
@@ -13,11 +13,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
         <UserProvider>
-            <ProductsProvider>
+            <CategoriesProvider>
                 <CartProvider>
                     <App />
                 </CartProvider>
-            </ProductsProvider>
+            </CategoriesProvider>
         </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,15 +1,17 @@
 import { useContext } from "react";
-import { ProductsContext } from "../../contexts/product";
-import ProductCard from "../../components/product_card/product_card";
+import { CategoriesContext } from "../../contexts/categories";
+import CategoryPreview from "../../components/category_preview/category_preview";
 import './shop.scss'
 
 const Shop = () =>{
-    const products  = useContext(ProductsContext)
+    const { categoriesMap }  = useContext(CategoriesContext)
+
     return (
-        <div className='products-container'>
-            {products.map((product) => (
-                <ProductCard product={product} key={product.id}/>
-                ))}
+        <div className='shop-container'>
+            {Object.keys(categoriesMap).map(title =>{
+                const products = categoriesMap[title]
+                return <CategoryPreview key={title} products={products} title={title}/>
+                })}
         </div>
     )
 }
