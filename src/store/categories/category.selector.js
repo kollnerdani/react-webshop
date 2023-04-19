5730d73b60a434d2fc475ff0497f639f64dface4
kollnerdani <dankollner@gmail.com>
2023-04-19 18:55:20 +0200

    reselect package

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,3 +1,5 @@
+import { createSelector } from "reselect";
+
 export const selectCategoriesMap = (state) => {
     const categoriesMap = state.categories.categories.reduce(
         (acc, { title, items }) => {
